71fda9b9b3bcb1753dd1c577016f8b5b24181530
Evelyn <137132532+evelynchang0605@users.noreply.github.com>
2023-07-25 16:46:29 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/recorderdemo/MainActivity.java
+++ app/src/main/java/com/example/recorderdemo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
         binding.setData(myViewModel);
